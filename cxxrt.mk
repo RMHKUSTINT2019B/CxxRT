99b70ceb7c4245ef9247cd76b3c767e36cb26572
DWVoid <yshliu0321@icloud.com>
2018-10-04 22:12:57 +0800

    Make Things Compile

---
+++ cxxrt.mk
@@ -0,0 +1,9 @@
+# List of all the licensing subsystem files.
+CXXRTSRC := $(CXXRT)/Src/_library.cpp
+
+# Required include directories
+CXXRTINC := $(CXXRT)/Include
+
+# Shared variables
+ALLCPPSRC += $(CXXRTSRC)
+ALLINC  += $(CXXRTINC)
