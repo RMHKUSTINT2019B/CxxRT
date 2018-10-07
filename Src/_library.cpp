4219172e6c0fa245599375ac9aa10edb71411dba
DWVoid <yshliu0321@icloud.com>
2018-10-07 16:52:18 +0800

    Fine Tuning 1

--- Src/_library.cpp
+++ Src/_library.cpp
@@ -1,3 +1,4 @@
+#include "../Include/chstd.hpp"
 #include "new.icc"
 #include "cxxabi.icc"
 #include "hal.h"
@@ -6,6 +7,12 @@ namespace chstd {
     void init() noexcept {
         halInit();
         chSysInit();
+        chstd::thread([]() {
+            for (;;) {
+                palTogglePad(GPIOA, GPIOA_LED);
+                chstd::this_thread::sleep_for(std::chrono::milliseconds(100));
+            }
+        }).detach();
     }
 
     namespace { struct AA { AA() noexcept { init(); }} _g_init_; }
